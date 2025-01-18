5c9481ce2baea2d5167f0d39bd236a39d5016f29
Ahlul Mukhramin <ahlul963@gmail.com>
2025-01-19 04:43:38 +0700

    feat: implement bot restart mechanism on error handling

--- index.js
+++ index.js
@@ -3,12 +3,38 @@ require("dotenv").config();
 const ChatBot = require("./src/ChatBot");
 const config = require("./src/utils/config");
 
-const client = new Client();
-new ChatBot(
-  client,
-  process.env.TARGET_CHANNEL_IDS.split(","),
-  process.env.CHATBOT_PROMPT,
-  config
-);
+function startBot() {
+  const client = new Client();
+  new ChatBot(
+    client,
+    process.env.TARGET_CHANNEL_IDS.split(","),
+    process.env.CHATBOT_PROMPT,
+    config
+  );
 
-client.login(process.env.DISCORD_TOKEN);
+  client.on("error", (error) => {
+    console.error("Client error:", error);
+    restartBot();
+  });
+
+  process.on("unhandledRejection", (error) => {
+    console.error("Unhandled promise rejection:", error);
+    restartBot();
+  });
+
+  process.on("uncaughtException", (error) => {
+    console.error("Uncaught exception:", error);
+    restartBot();
+  });
+
+  client.login(process.env.DISCORD_TOKEN);
+}
+
+function restartBot() {
+  console.log("Restarting bot in 5 seconds...");
+  setTimeout(() => {
+    startBot();
+  }, 5000);
+}
+
+startBot();
